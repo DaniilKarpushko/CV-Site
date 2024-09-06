0ffb100c4020a401787dc8828cbfb82eaf2a1777
DaniilKarpushko <steamovsky525@mail.ru>
2024-09-06 17:02:35 +0300

    feat: sec page done

--- CV-Site/CV-Site/Program.cs
+++ CV-Site/CV-Site/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+        .AddInteractiveServerComponents();
+
 
 var app = builder.Build();
 
